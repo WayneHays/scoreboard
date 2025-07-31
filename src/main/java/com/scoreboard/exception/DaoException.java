d726be14aa92c53ee2aa5dd00c67882b3501bb4e
WayneHays <waynehays1605@gmail.com>
2025-07-31 15:36:46 +0300

    feat(exception): add DaoException and EntityExistsException

---
+++ src/main/java/com/scoreboard/exception/DaoException.java
@@ -0,0 +1,7 @@
+package com.scoreboard.exception;
+
+public class DaoException extends RuntimeException {
+
+    public DaoException(String message) {
+    }
+}
---
+++ src/main/java/com/scoreboard/exception/EntityExistsException.java
@@ -0,0 +1,7 @@
+package com.scoreboard.exception;
+
+public class EntityExistsException extends RuntimeException {
+
+    public EntityExistsException(String message) {
+    }
+}
