85a1e95dd7661705b1a2b9f563905a1fd4e1140b
WayneHays <waynehays1605@gmail.com>
2025-08-01 11:57:33 +0300

    refactor: make Match and Player hibernate models

--- src/main/java/com/scoreboard/model/Match.java
+++ src/main/java/com/scoreboard/model/Match.java
@@ -1,16 +1,32 @@
-package com.scoreboard.entity;
+package com.scoreboard.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Getter
+@Setter
+
+@Entity
+@Table(name = "Matches")
 public class Match {
+
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "player1")
     private Long firstPlayerId;
+
+    @Column(name = "player2")
     private Long secondPlayerId;
+
+    @Column(name = "winner")
     private Long winnerId;
 
     public Match(Long firstPlayerId, Long secondPlayerId, Long winnerId) {
--- src/main/java/com/scoreboard/model/Player.java
+++ src/main/java/com/scoreboard/model/Player.java
@@ -1,12 +1,22 @@
-package com.scoreboard.entity;
+package com.scoreboard.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Setter
+@Getter
+
+@Entity
+@Table (name = "Players")
 public class Player {
+    @Id
+    @Column (name = "id")
     private Long id;
+
+    @Column (name = "name")
     private String name;
 
     public Player(String name) {
