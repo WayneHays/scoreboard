0b15431414656f063ddd843b4de942948a3011e0
WayneHays <waynehays1605@gmail.com>
2025-08-05 10:31:19 +0300

    refactor: PlayersDao -> refactor "save" method, add "findByName" method

--- src/main/java/com/scoreboard/dao/PlayersDao.java
+++ src/main/java/com/scoreboard/dao/PlayersDao.java
@@ -1,14 +1,28 @@
 package com.scoreboard.dao;
 
+import com.scoreboard.exception.DaoException;
 import com.scoreboard.model.Player;
 import com.scoreboard.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.exception.ConstraintViolationException;
 
 public class PlayersDao {
 
-    public Player save(Player player) {
-      Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-      session.persist(player);
-      return player;
+    public void save(Player player) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.persist(player);
+        } catch (Exception e) {
+            throw new DaoException("Failed to create player", e);
+        }
+    }
+
+    public Player findByName(Player player) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            return session.find(Player.class, player);
+        } catch (Exception e) {
+            throw new DaoException("Failed to find player", e);
+        }
     }
 }
