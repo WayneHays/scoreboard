9ee1c2673b8f63924bdf8f6bc6a38c430bb51fd3
WayneHays <waynehays1605@gmail.com>
2025-08-06 11:08:42 +0300

    feat: PlayerService -> add "findById" method

--- src/main/java/com/scoreboard/service/PlayerService.java
+++ src/main/java/com/scoreboard/service/PlayerService.java
@@ -2,6 +2,8 @@ package com.scoreboard.service;
 
 import com.scoreboard.dao.PlayersDao;
 import com.scoreboard.exception.DaoException;
+import com.scoreboard.exception.NotFoundException;
+import com.scoreboard.exception.ScoreboardServiceException;
 import com.scoreboard.model.Player;
 import com.scoreboard.util.HibernateUtil;
 import org.hibernate.Session;
@@ -10,10 +12,15 @@ import org.hibernate.Transaction;
 import java.util.Optional;
 
 public class PlayerService {
-    private PlayersDao playersDao;
+    private static final PlayerService INSTANCE = new PlayerService();
+    private final PlayersDao playersDao;
 
     public PlayerService() {
         this.playersDao = new PlayersDao();
+    }
+
+    public static PlayerService getInstance() {
+        return INSTANCE;
     }
 
     public Player create(String name) {
@@ -29,6 +36,19 @@ public class PlayerService {
         } catch (DaoException e) {
             transaction.rollback();
             throw new RuntimeException("Failed to create player", e);
+        }
+    }
+
+    public Player findById(Long id) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Optional<Player> found = playersDao.findById(id);
+            transaction.commit();
+            return found.orElseThrow(() -> new NotFoundException("Player with id %d not found".formatted(id)));
+        } catch (DaoException e) {
+            throw new ScoreboardServiceException("Failed to find player by id: %d".formatted(id), e);
         }
     }
 }
