fa68c021e33cd6fb695f096d3c69b0587ffe3c26
WayneHays <waynehays1605@gmail.com>
2025-08-06 11:06:09 +0300

    refactor: MatchWithScore -> class fields

--- src/main/java/com/scoreboard/model/MatchWithScore.java
+++ src/main/java/com/scoreboard/model/MatchWithScore.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 @Setter
 
 public class MatchWithScore {
-    private Player firstPlayer;
-    private Player secondPlayer;
+    private Match match;
     private Score score;
 }
