ae6efe1d8cddeb9504de028ce0e7bee2876801e7
WayneHays <waynehays1605@gmail.com>
2025-08-06 11:03:54 +0300

    feat: add FinishedMatchService

---
+++ src/main/java/com/scoreboard/service/FinishedMatchService.java
@@ -0,0 +1,32 @@
+package com.scoreboard.service;
+
+import com.scoreboard.dao.MatchDao;
+import com.scoreboard.exception.DaoException;
+import com.scoreboard.exception.ScoreboardServiceException;
+import com.scoreboard.model.Match;
+import com.scoreboard.model.MatchWithScore;
+import com.scoreboard.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class FinishedMatchService {
+    private static final FinishedMatchService INSTANCE = new FinishedMatchService();
+    private final MatchDao matchDao = new MatchDao();
+
+    public static FinishedMatchService getInstance() {
+        return INSTANCE;
+    }
+
+    public void saveToDatabase(MatchWithScore matchWithScore) {
+        Match match = matchWithScore.getMatch();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            matchDao.save(match);
+            transaction.commit();
+        } catch (DaoException e) {
+            throw new ScoreboardServiceException("Failed to save match in database", e);
+        }
+    }
+}
