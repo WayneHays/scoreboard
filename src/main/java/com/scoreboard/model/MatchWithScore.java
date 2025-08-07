afa319df0594655d74ab1628aaf9f31986fe4424
WayneHays <waynehays1605@gmail.com>
2025-08-07 08:56:48 +0300

    refactor: MatchWithScore -> record

--- src/main/java/com/scoreboard/model/MatchWithScore.java
+++ src/main/java/com/scoreboard/model/MatchWithScore.java
@@ -1,14 +1,4 @@
 package com.scoreboard.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Getter
-@Setter
-
-public class MatchWithScore {
-    private Match match;
-    private Score score;
+public record MatchWithScore(Match match, Score score) {
 }
