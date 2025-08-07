3e27de38e0e43853a10ce4cbca8b99447d6011fa
WayneHays <waynehays1605@gmail.com>
2025-08-07 08:59:08 +0300

    refactor: Score.class

--- src/main/java/com/scoreboard/model/Score.java
+++ src/main/java/com/scoreboard/model/Score.java
@@ -1,20 +1,74 @@
 package com.scoreboard.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
-
 public class Score {
-    private int firstPlayerPoints;
-    private int firstPlayerGames;
-    private int firstPlayerSets;
-    private int secondPlayerPoints;
-    private int secondPlayerGames;
-    private int secondPlayerSets;
+    private final Map<Player, Integer> playersPoints;
+    private final Map<Player, Integer> playersGames;
+    private final Map<Player, Integer> playersSets;
+    private final Map<Player, Integer> playersTieBreakPoints;
+    private Player advantage;
+    private boolean isDeuce;
+    private boolean isTieBreak;
+    private boolean isMatchFinished;
+
+    public Score() {
+        this.playersTieBreakPoints = new HashMap<>();
+        this.playersSets = new HashMap<>();
+        this.playersGames = new HashMap<>();
+        this.playersPoints = new HashMap<>();
+    }
+
+    public int getPoints(Player player) {
+        return playersPoints.get(player);
+    }
+
+    public int getGames(Player player) {
+        return playersGames.get(player);
+    }
+
+    public int getSets(Player player) {
+        return playersSets.get(player);
+    }
+
+    public int getTieBreakPoints(Player player) {
+        return playersTieBreakPoints.get(player);
+    }
+
+    public void setPoints(Player player, int points) {
+        playersPoints.put(player, points);
+    }
+
+    public void setGames(Player player, int games) {
+        playersGames.put(player, games);
+    }
+
+    public void setSets(Player player, int sets) {
+        playersSets.put(player, sets);
+    }
+
+    public void setTieBreakPoints(Player player, int points) {
+        playersTieBreakPoints.put(player, points);
+    }
+
+    public void resetPoints(Player firstPlayer, Player secondPlayer) {
+        playersPoints.put(firstPlayer, 0);
+        playersPoints.put(secondPlayer, 0);
+    }
+
+    public void resetGames(Player firstPlayer, Player secondPlayer) {
+        playersGames.put(firstPlayer, 0);
+        playersGames.put(secondPlayer, 0);
+    }
+
+    public void resetTieBreakPoints (Player firstPlayer, Player secondPlayer) {
+        playersTieBreakPoints.put(firstPlayer, 0);
+        playersTieBreakPoints.put(secondPlayer, 0);
+    }
 }
