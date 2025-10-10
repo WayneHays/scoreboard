3cf81c56c790fe37cf24ee370fb949a7a7c7dffe
WayneHays <waynehays1605@gmail.com>
2025-10-10 16:25:32 +0300

    refactor: getPlayers() -> getPlayerNames()

--- src/main/java/com/scoreboard/start_initialization/data_source/DataSource.java
+++ src/main/java/com/scoreboard/start_initialization/data_source/DataSource.java
@@ -3,5 +3,5 @@ package com.scoreboard.start_initialization.data_source;
 import java.util.List;
 
 public interface DataSource {
-    List<String> getPlayers();
+    List<String> getPlayerNames();
 }
