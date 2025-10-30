677875d0295fad0df53e033530698c7f82afaec5
WayneHays <waynehays1605@gmail.com>
2025-10-30 11:39:42 +0300

    feat: add ApplicationStartupException

--- src/main/java/com/scoreboard/exception/ApplicationStartupException.java
+++ src/main/java/com/scoreboard/exception/ApplicationStartupException.java
@@ -2,6 +2,10 @@ package com.scoreboard.exception;
 
 public class ApplicationStartupException extends RuntimeException {
 
+    public ApplicationStartupException(String message) {
+        super(message);
+    }
+
     public ApplicationStartupException(String message, Throwable cause) {
         super(message, cause);
     }
