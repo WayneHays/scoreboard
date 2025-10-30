4f18723384ab67a99de189dd51a29c8ab809fbe0
WayneHays <waynehays1605@gmail.com>
2025-10-30 11:22:12 +0300

    refactor: removed old tests for score calculation

--- src/main/java/com/scoreboard/service/BaseTransactionalService.java
+++ src/main/java/com/scoreboard/service/BaseTransactionalService.java
@@ -1,6 +1,8 @@
 package com.scoreboard.service;
 
+import com.scoreboard.exception.NotFoundException;
 import com.scoreboard.exception.ScoreboardServiceException;
+import com.scoreboard.exception.ValidationException;
 import com.scoreboard.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -8,7 +10,19 @@ import org.hibernate.Transaction;
 import java.util.function.Supplier;
 
 public abstract class BaseTransactionalService {
+
     protected <T> T executeInTransaction(Supplier<T> operation, String errorMessage) {
+        return executeCommon(operation, errorMessage);
+    }
+
+    protected void executeInTransaction(Runnable operation, String errorMessage) {
+        executeCommon(() -> {
+            operation.run();
+            return null;
+        }, errorMessage);
+    }
+
+    private <T> T executeCommon(Supplier<T> operation, String errorMessage) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         Transaction transaction = session.beginTransaction();
 
@@ -16,19 +30,11 @@ public abstract class BaseTransactionalService {
             T result = operation.get();
             transaction.commit();
             return result;
-        } catch (RuntimeException e) {
+
+        } catch (NotFoundException | ValidationException e) {
             transaction.rollback();
-            throw new ScoreboardServiceException(errorMessage, e);
-        }
-    }
+            throw e;
 
-    protected void executeInTransaction(Runnable operation, String errorMessage) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-
-        try {
-            operation.run();
-            transaction.commit();
         } catch (RuntimeException e) {
             transaction.rollback();
             throw new ScoreboardServiceException(errorMessage, e);
