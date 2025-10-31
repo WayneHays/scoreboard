e2352e27497c7c598488df3a1eed22da118efe61
WayneHays <waynehays1605@gmail.com>
2025-10-31 20:29:15 +0300

    refactor: BaseDao -> BaseHibernateDao

--- src/main/java/com/scoreboard/dao/PlayerDaoImpl.java
+++ src/main/java/com/scoreboard/dao/PlayerDaoImpl.java
@@ -4,7 +4,7 @@ import com.scoreboard.model.entity.Player;
 
 import java.util.Optional;
 
-public class PlayerDaoImpl extends BaseDao<Player> implements PlayerDao {
+public class PlayerDaoImpl extends BaseHibernateDao<Player> implements PlayerDao {
     private static final String FIND_BY_NAME = "FROM Player WHERE LOWER(name) = LOWER(:name)";
 
     public Optional<Player> find(String name) {
